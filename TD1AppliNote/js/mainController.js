ebdd6ca3ad578c7ef17f9091dfc4bb7a11b8b3c5
ArmandBaglin <21501022@etu.unicaen.fr>
2017-02-03 09:24:05 +0100

    TD1ApplicationNote
    Deuxième version, il y a encore une erreur d'affichage au début, le
    {{mainCtrl.count()}} est affiché dans la page mais il n'empêche pas
    l'éxécution.

--- TD1AppliNote/js/mainController.js
+++ TD1AppliNote/js/mainController.js
@@ -1,6 +1,35 @@
 /**
  * Created by Utilisateur on 27/01/2017.
  */
-angular.module("ApplicationNote").controller("MainController", function(){
+var app=angular.module("ApplicationNote",["ngCookies"]);
+app.controller("MainController", ["$cookies", function(cookies){
 
-})
+    var message="";
+    this.value=cookies.get('messageCookie')
+    var self=this;
+
+    this.count = function(){
+        if(self.value!="" && self.value != cookies.get('messageCookie'))
+            message="Message modifié";
+
+        return 100-self.value.length;
+    }
+
+    this.clear = function(){
+
+        message="";
+
+        self.value="";
+    }
+
+    this.save = function(){
+        message="Message sauvegardé";
+        if(self.value != "")
+            cookies.put('messageCookie',self.value);
+    }
+
+    this.show = function(){
+
+        return message;
+    }
+}]);
