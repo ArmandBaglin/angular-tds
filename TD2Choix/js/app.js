16af38d422482559fe6c1624c6fd8ba59ace6912
ArmandBaglin <21501022@etu.unicaen.fr>
2017-02-07 11:00:55 +0100

    TD2
    Début de l'exercice 1 du TD2 sur les choix multiples, liaison de l'html
    au javascript

---
+++ TD2Choix/js/app.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Utilisateur on 07/02/2017.
+ */
+angular.module("ApplicationChoix",[]);
---
+++ TD2Choix/js/controllerChoix.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Utilisateur on 07/02/2017.
+ */
+var app=angular.module("ApplicationChoix").controller("ControllerChoix", [ function(){
+
+}]);
