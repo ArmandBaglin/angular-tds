f5ee017077e25cf4d016678914a08fd4f305bbc1
ArmandBaglin <21501022@etu.unicaen.fr>
2017-02-17 12:03:46 +0100

    TD2ContactsV2
    Intégration de plusieurs fonctions js et du filtrer gérant les contacts
    supprimés ou non

--- TD2Contacts/js/Controller.js
+++ TD2Contacts/js/Controller.js
@@ -7,15 +7,72 @@ angular.module("ApplicationContacts").controller("ControllerContacts", [ functio
         {
             nom:"ZUCKENBERG",
             prenom:"Mark",
-            email:"mark@facebook.com"
+            email:"mark@facebook.com",
+            visible:true
         },{
             nom:"GATES",
             prenom:"Bill",
-            email:"bill@microsoft.com"
+            email:"bill@microsoft.com",
+            visible:true
         },{
             nom:"JOBS",
             prenom:"Steve",
-            email:"steve@apple.com"
+            email:"steve@apple.com",
+            visible:true
         }
     ];
+    this.ajout=false;
+    this.modif=false;
+    this.ajoutVisible= function(){
+        return this.ajout;
+    }
+
+    this.ajoutClick= function(){
+        this.ajout=true;
+        return this.ajout;
+    }
+
+    this.modifVisible= function(){
+        return this.modif;
+    }
+
+    this.modifClick= function(){
+        this.modif=true;
+        return this.modif;
+    }
+
+    this.annulerModif=function(){
+        this.modif=false;
+        return this.modif;
+    }
+
+    this.annulerAjout = function(){
+        this.ajout=false;
+        return this.ajout;
+    }
+
+    this.voirContact= function(visible){
+        return visible;
+    }
+
+    this.delete= function(contact){
+        contact.visible=false;
+        return contact.visible;
+    }
+
 }]);
+
+angular.module("ApplicationContacts").filter('visible',function(){
+    return function(contacts){
+        var results=[];
+        for(var i=0;i<contacts.length;i++){
+            if(contacts[i].visible==true){
+                results.push(contacts[i]);
+            }
+        }
+        return results;
+    }
+});
+
+
+
