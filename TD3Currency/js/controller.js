c41dbcc65f7c0a9011fd8aa97591e6f5c6fad24b
ArmandBaglin <21501022@etu.unicaen.fr>
2017-03-03 09:37:31 +0100

    TD3Currency
    Modification de l'exercice 1 du TD3, pour le moment, j'essaye d'afficher
    les informations dans les deux listes déroulantes à l'aide de ng-options
    et ng-model

--- TD3Currency/js/controller.js
+++ TD3Currency/js/controller.js
@@ -1,7 +1,7 @@
 /**
  * Created by Utilisateur on 03/03/2017.
  */
-angular.module("ApplicationCurrency").controller("ControllerCurrency", [ function(){
+angular.module("ApplicationCurrency").controller("ControllerCurrency", [ '$http', function($http){
 
     $http.get('app/data/currencymap.json').
     then(function(response) {
@@ -11,8 +11,8 @@ angular.module("ApplicationCurrency").controller("ControllerCurrency", [ functio
             console.log("Erreur avec le statut Http : "+response.status);
         });
 
-    $http.jsonp('https://free.currencyconverterapi.com/api/v3/convert?compact=y&q='+from.code+'_'+to.code, {jsonpCallbackParam: 'callback'})
+    /*$http.jsonp('https://free.currencyconverterapi.com/api/v3/convert?compact=y&q='+from.code+'_'+to.code, {jsonpCallbackParam: 'callback'})
         .then(function(response) {
             self.result=response.data[self.from.code+'_'+self.to.code].val;
-        });
+        });*/
 }]);
